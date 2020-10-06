0f1a88b8dd8e1b15caf5deea05da8ab40caafdcf
Balbaal <iqbal.mi20@gmail.com>
2020-10-06 13:22:20 +0800

    refactor: adjust input decision type

--- src/elements/Input.js
+++ src/elements/Input.js
@@ -1,15 +1,31 @@
 import React from "react";
 
 const Input = (props) => {
+  if (props.type !== "textarea") {
+    return (
+      <input
+        type={props.type}
+        name={props.name}
+        className={[
+          props.type === "file" ? "form-control-file" : "form-control",
+          props.className,
+        ].join(" ")}
+        placeholder={props.placeholder}
+        value={props.value}
+        onChange={props.onChange}
+      />
+    );
+  }
+
   return (
-    <input
-      type={props.type}
-      name={props.name}
-      className={["form-control", props.className].join(" ")}
-      placeholder={props.placeholder}
+    <textarea
       value={props.value}
+      name={props.name}
+      placeholder={props.placeholder}
       onChange={props.onChange}
-    />
+      className={["form-control", props.className].join(" ")}
+      rows="5"
+    ></textarea>
   );
 };
 
