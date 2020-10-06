010d8256634f3e4fe1bbcd393ec8f72afd9793c8
Balbaal <iqbal.mi20@gmail.com>
2020-10-06 15:17:35 +0800

    chore: axios custom baseurl

---
+++ src/configs/axios/index.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: `${process.env.REACT_APP_API_EVENT}/api/v1`,
+});
+
+instance.interceptors.response.use(
+  (res) => res.data,
+  (err) => Promise.reject(error)
+);
+
+export default instance;
